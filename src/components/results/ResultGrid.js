cee50a1111000284f0df6cfc921b37c3ae3398b4
bilawal-cyber <bilawala707@gmail.com>
2021-09-28 00:28:34 +0500

    add data table

--- src/components/results/ResultGrid.js
+++ src/components/results/ResultGrid.js
@@ -1,99 +1,14 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
-// import List from '@material-ui/core/List';
-// import AddQuestion from './AddQuestion'
-// import AddOptions from './addOptions';
-// import {Addicon,AddQuestionButton} from '../Buttons';
-// import axios  from 'axios';
 import { Grid } from '@material-ui/core';
-// import  LinearProgress  from '@material-ui/core/LinearProgress';
-
-// const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: 'flex',
-//   },
-//   toolbar: {
-//     paddingRight: 24, // keep right padding when drawer closed
-//   },
-//   toolbarIcon: {
-//     display: 'flex',
-//     alignItems: 'center',
-//     justifyContent: 'flex-end',
-//     padding: '0 8px',
-//     ...theme.mixins.toolbar,
-//   },
-//   appBar: {
-//     zIndex: theme.zIndex.drawer + 1,
-//     transition: theme.transitions.create(['width', 'margin'], {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.leavingScreen,
-//     }),
-//   },
-//   appBarShift: {
-//     marginLeft: drawerWidth,
-//     width: `calc(100% - ${drawerWidth}px)`,
-//     transition: theme.transitions.create(['width', 'margin'], {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//   },
-//   menuButton: {
-//     marginRight: 36,
-//   },
-//   menuButtonHidden: {
-//     display: 'none',
-//   },
-//   title: {
-//     flexGrow: 1,
-//   },
-//   drawerPaper: {
-//     position: 'relative',
-//     whiteSpace: 'nowrap',
-//     width: drawerWidth,
-//     transition: theme.transitions.create('width', {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//   },
-//   drawerPaperClose: {
-//     overflowX: 'hidden',
-//     transition: theme.transitions.create('width', {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.leavingScreen,
-//     }),
-//     width: theme.spacing(7),
-//     [theme.breakpoints.up('sm')]: {
-//       width: theme.spacing(9),
-//     },
-//   },
-//   appBarSpacer: theme.mixins.toolbar,
-//   content: {
-//     flexGrow: 1,
-//     height: '100vh',
-//     overflow: 'auto',
-//   },
-//   container: {
-//     paddingTop: theme.spacing(4),
-//     paddingBottom: theme.spacing(4),
-//   },
-//   paper: {
-//     padding: theme.spacing(2),
-//     display: 'flex',
-//     overflow: 'auto',
-//     flexDirection: 'column',
-//   },
-//   fixedHeight: {
-//     height: 240,
-//   },
-}));
-
-
-
-
-
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 export default function ResultGrid() {
 
@@ -103,15 +18,56 @@ export default function ResultGrid() {
     }
 
 
-//   const classes = useStyles();
+    function createData(name, calories, fat, carbs, protein) {
+      return { name, calories, fat, carbs, protein };
+    }
+    
+    const rows = [
+      createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+      createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+      createData('Eclair', 262, 16.0, 24, 6.0),
+      createData('Cupcake', 305, 3.7, 67, 4.3),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+    ];
 
-
+    const useStyles = makeStyles({
+      table: {
+        minWidth: 650,
+      },
+    });
+    const classes = useStyles();
 
   return (
     <Grid item  >
     <Box   p={3} 
      sx={{ borderRadius: 16 }} style={box}>
-      <h3>Final Results</h3>
+      <TableContainer component={Paper} style={{background:'#d1d9ff'}}>
+      <h3>your score</h3>
+      <Table className={classes.table}  aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </Box>
     </Grid>
   );
