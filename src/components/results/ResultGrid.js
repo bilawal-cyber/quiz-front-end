38201675ff732936f549b97e907bb78497b428bf
bilawal-cyber <bilawala707@gmail.com>
2021-09-29 00:44:31 +0500

    show results

--- src/components/results/ResultGrid.js
+++ src/components/results/ResultGrid.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import { Grid } from '@material-ui/core';
@@ -9,15 +9,19 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import axios from 'axios';
 
-export default function ResultGrid({result}) {
-
+export default function ResultGrid() {
+const [result,setResults] = useState([])
     const box={
       background: "#d1d9ff",
       border:"1px solid rgb(19, 47, 76)"
     }
 
-
+useEffect(()=>{
+    axios.get('http://127.0.0.1:5000/api/userData').then(res=>setResults([...result,...res.data]))
+    .catch(err=>console.log(err))
+},[])
     // function createData(name, calories, fat, carbs, protein) {
     //   return { name, calories, fat, carbs, protein };
     // }
@@ -40,11 +44,12 @@ export default function ResultGrid({result}) {
 
   return (
     <Grid item  >
-    {result.length ?
-    <Box   p={3} 
+    {
+      (result.length)?
+      <Box   p={3} 
     sx={{ borderRadius: 16 }} style={box}>
      <TableContainer component={Paper} style={{background:'#d1d9ff'}}>
-     <h3>your score </h3>
+     <h3>your score {result[0].score}</h3>
      <Table className={classes.table}  aria-label="simple table">
        <TableHead>
          <TableRow>
@@ -54,20 +59,23 @@ export default function ResultGrid({result}) {
          </TableRow>
        </TableHead>
        <TableBody>
-         {result.map((r) => (
-           <TableRow key={r._id}>
-             <TableCell component="th" scope="row">
-               <h5>hel</h5>
-             </TableCell>
-             {/* <TableCell align="right">{r.answer_id.option}</TableCell>
-             <TableCell align="right">{r.is_correct}</TableCell> */}
-           </TableRow>
-         ))}
+           {
+             result[0].userAnwers.map(e=>(
+              <TableRow key={e._id}>
+              <TableCell component="th" scope="row">
+                <h5>{e.question_id.question}</h5>
+              </TableCell>
+              <TableCell align="right">{(e.answer_id)?e.answer_id.option:JSON.stringify(!e.is_correct)}</TableCell>
+              <TableCell align="right">{JSON.stringify(e.is_correct)}</TableCell>
+            </TableRow>
+             ))
+           }
+
        </TableBody>
      </Table>
    </TableContainer>
-   </Box> : ''
-   }
+   </Box>:''
+    }
     </Grid>
   );
 }
